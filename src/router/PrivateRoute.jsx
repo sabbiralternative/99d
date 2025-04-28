f9c99940f834c882ebae634e4fb3472f77a5d5d2
sabbirHossain <sabbirshnt@gmail.com>
2025-04-28 16:02:38 +0600

    done

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -2,15 +2,10 @@ import { Navigate } from "react-router-dom";
 import { Settings } from "../api";
 import { logout } from "../redux/features/auth/authSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { isMobile } from "react-device-detect";
 
 const PrivateRoute = ({ children }) => {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
-
-  if (isMobile) {
-    window.location.href = "/m/";
-  }
 
   if (Settings.forceLogin) {
     if (!token) {
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import notice from "./notice.json";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: notice?.result?.settings?.baseUrl || "/",
+  base: notice?.result?.settings?.baseUrl || "/d/",
 });
