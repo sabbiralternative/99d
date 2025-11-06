d61bbcbea21c3afb0610d47aef8963b7e4465353
sabbirHossain <sabbirshnt@gmail.com>
2025-11-06 17:06:55 +0600

    done

--- src/components/modules/Deposit/BankAccountUploadTransaction.jsx
+++ src/components/modules/Deposit/BankAccountUploadTransaction.jsx
@@ -41,10 +41,11 @@ const BankAccountUploadTransaction = ({ setTab, amount }) => {
     setMethodType(method?.type);
     setPaymentId(method?.paymentId);
 
-    if (method?.type === "upigateway") {
+    if (method?.type === "upigateway" || method?.type === "toitgateway") {
       const depositDetailForPg = {
         paymentId: method?.paymentId,
         amount,
+        method: method?.type,
       };
       const res = await AxiosSecure.post(API.pg, depositDetailForPg);
       const data = res?.data;
@@ -246,7 +247,8 @@ const BankAccountUploadTransaction = ({ setTab, amount }) => {
                                 src={images.whatsApp}
                               />
                             ) : null}
-                            {method?.type == "upigateway" ? (
+                            {method?.type == "upigateway" ||
+                            method?.type === "toitgateway" ? (
                               <img
                                 style={{
                                   height: "20px",
